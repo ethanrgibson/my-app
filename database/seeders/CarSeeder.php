9880dd9903afdeb604e5258e4cdbccb7d65ad705
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-26 13:58:28 -0600

    got 6 cars in DB

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -14,12 +14,12 @@ class CarSeeder extends Seeder
     public function run(): void
     {
         DB::table('cars')->insert([
-            'make'=>'toyota',
-            'model'=>'tacoma',
-            'description'=>'Love my coma, never want another car ever!',
-            'price'=>1000,
-            'img_url' => 'https://images.unsplash.com/photo-1583031849506-4f35952c3fa0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            'year'=> 2025,
+            'make'=>'volvo',
+            'model'=>'big dawg',
+            'description'=>'18 wheels and slow moving',
+            'price'=>750000,
+            'img_url' => 'https://images.unsplash.com/photo-1501700493788-fa1a4fc9fe62?q=80&w=1989&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'year'=> 2014,
             'is_published' => true,
             'created_at'=> now(),
             'updated_at' => now()
