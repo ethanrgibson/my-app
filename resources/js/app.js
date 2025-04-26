c4e9d58260a8e4d4767b6ee18838c5f628d2616f
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-25 20:07:42 -0600

    trying to get vue compnent to page

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import HelloWorld from './Components/HelloWorld.vue'
+
+createApp({
+  components: {
+    HelloWorld
+  }
+}).mount('#app')
---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <div class="p-4 text-xl text-green-600">
+    👋 Hello from Vue!
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
